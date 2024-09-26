94f1ac02025dfda050d6fac0f9cdb66c8fb4c0b5
imorochi <imorochi.dev@gmail.com>
2024-09-26 17:08:34 -0400

    2. Add api gateway

---
+++ istore-api-gateway/src/main/java/bo/imorochi/microservice/istore/api/gateway/Application.java
@@ -0,0 +1,15 @@
+package bo.imorochi.microservice.istore.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
