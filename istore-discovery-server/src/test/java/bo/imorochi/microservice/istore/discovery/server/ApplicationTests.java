79837918d069231394301ab2536a007463943de1
imorochi <imorochi.dev@gmail.com>
2024-09-26 17:07:15 -0400

    1. Add discovery service

---
+++ istore-discovery-server/src/test/java/bo/imorochi/microservice/istore/discovery/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.imorochi.microservice.istore.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
